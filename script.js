b7e11c249ef798c60591774842dba893bac09e73
Priscila Souza <32541782+prikasouza@users.noreply.github.com>
2023-07-26 03:03:09 -0300

    new model card

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-;<script>AOS.init();</script>
+var button = document.getElementById('card-btn');
+
+button.addEventListener('click', function() {
+    
+    var card = document.querySelector('.card');
+
+    
+    card.classList.toggle('active');
+
+    if (card.classList.contains('active')) {
+    
+        return button.textContent = 'Ver Menos';
+    }
+    
+    
+    button.textContent = 'Saiba Mais';
+});
